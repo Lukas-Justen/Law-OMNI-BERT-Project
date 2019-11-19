b167f45df926fce04a08f8aa6b82a287c566e250
Lukas Justen <lukasjusten@gmail.com>
2019-11-19 17:30:57 -0600

    Add bar chart

--- src/frontend/src/components/VisualizerApp/index.js
+++ src/frontend/src/components/VisualizerApp/index.js
@@ -5,38 +5,61 @@ import WordFrequencyGraph from "../WordFrequencyGraph";
 
 class VisualizerApp extends React.Component {
 
-  render() {
-    return (
-        <div className={"h-100"}>
+    render() {
+        return (
+            <div className={"h-100"}>
 
-            <MDBNavbar color="white" light expand="md">
-                <MDBNavbarNav left>
-                    <MDBNavbarBrand>
-                        <a href={"https://www.mccormick.northwestern.edu/artificial-intelligence/"}>
-                            <img
-                                src="https://cdn.bleacherreport.net/images/team_logos/328x328/northwestern_football.png"
-                                alt="Avatar"
-                                className="md-avatar" height={32.0}/>
-                        </a>
-                        <strong className="black-text">BERT Corpus Visualizer</strong>
-                    </MDBNavbarBrand>
-                </MDBNavbarNav>
-                <MDBNavbarNav right>
-                    <MDBBtn className={"upload-btn shadow-none"}>Upload document</MDBBtn>
-                </MDBNavbarNav>
-            </MDBNavbar>
+                <MDBNavbar color="white" light expand="md">
+                    <MDBNavbarNav left>
+                        <MDBNavbarBrand>
+                            <a href={"https://www.mccormick.northwestern.edu/artificial-intelligence/"}>
+                                <img
+                                    src="https://cdn.bleacherreport.net/images/team_logos/328x328/northwestern_football.png"
+                                    alt="Avatar"
+                                    className="md-avatar" height={32.0}/>
+                            </a>
+                            <strong className="black-text">BERT Corpus Visualizer</strong>
+                        </MDBNavbarBrand>
+                    </MDBNavbarNav>
+                    <MDBNavbarNav right>
+                        <MDBBtn color={"primary"} className={"upload-btn shadow-none"}>Upload document</MDBBtn>
+                    </MDBNavbarNav>
+                </MDBNavbar>
 
-            <div className={"dashboard"}>
+                <div className={"dashboard"}>
 
-                <MDBCard className={"general-stats"}></MDBCard>
-                <MDBCard className={"visualization"}>
-                    <WordFrequencyGraph id={"word-frequency"} data={[{"train":0.5, "test":0.6}, {"train": 5, "test": 20}]} height={"100%"}/>
-                </MDBCard>
+                    <MDBCard className={"general-stats"}></MDBCard>
+                    <MDBCard className={"visualization"}>
+                        <h5 className={"content-headline"}><i className="fas fa-chart-line content-icon"/>
+                            <b>Word Frequency Graph</b></h5>
+                        <WordFrequencyGraph id={"word-frequency"} data={[
+                            {"word": "Hello", "freq": 120},
+                            {"word": "sfdsdf", "freq": 100},
+                            {"word": "Hesdfdllo", "freq": 100},
+                            {"word": "Hedllo", "freq": 100},
+                            {"word": "LOL", "freq": 83},
+                            {"word": "Test", "freq": 80},
+                            {"word": "7", "freq": 80},
+                            {"word": "Te676st", "freq": 80},
+                            {"word": "676", "freq": 80},
+                            {"word": "ghg", "freq": 80},
+                            {"word": "Na", "freq": 80},
+                            {"word": "Yes","freq": 79},
+                            {"word": "dfjhs", "freq": 55},
+                            {"word": "dfvds", "freq": 48},
+                            {"word": "sdf", "freq": 44},
+                            {"word": "sdfsd","freq": 40},
+                            {"word": "ghngg", "freq": 39},
+                            {"word": "ggg", "freq": 39},
+                            {"word": "hhh", "freq": 20},
+                            {"word": "ghnhjhj","freq": 10},
+                            {"word": "Why", "freq": 5}]} height={"calc(100% - 60px)"}/>
+                    </MDBCard>
 
+                </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 
 }
 
--- src/frontend/src/components/WordFrequencyGraph/index.js
+++ src/frontend/src/components/WordFrequencyGraph/index.js
@@ -44,13 +44,11 @@ class WordFrequencyGraph extends React.Component {
 
     // Draws the svg and all child components
     drawChart(animate) {
-        // Transform the data
-        const trainingData = this.props.data.map(x => {
-            return {y: x.train}
+        // Transform the corpus1
+        const corpus1 = this.props.data.map(o => {
+            return {x: o.word, y: o.freq}
         });
-        const validationData = this.props.data.map(x => {
-            return {y: x.test}
-        });
+        const corpus2 = corpus1;
 
         // Define the parent div and svg
         const parent = d3.select("." + this.props.id);
@@ -58,59 +56,28 @@ class WordFrequencyGraph extends React.Component {
         const svg = this.getSvg(parent);
 
         // Define the axis and the gridlines
-        const x = this.getX(trainingData, margin);
-        const y = this.getY(trainingData, validationData, margin);
+        const x = this.getX(corpus1, margin);
+        const y = this.getY(corpus1, corpus2, margin);
         const xTicks = this.getXTicks();
         const yTicks = this.getYTicks();
         this.getXAxis(x, xTicks, margin, svg);
         this.getYAxis(y, yTicks, margin, svg);
         this.getGridlinesY(svg, margin, y, yTicks);
 
-        var area = d3.area()
-            .x(function(d, i) { return x(i); })
-            .y0(this.props.height)
-            .y1(function(d, i) { return y(d.y); });
-
-        svg.append("path")
-            .data(trainingData)
-            .attr("class", "area")
-            .attr("d", area);
-
-        // Build a legend for training and testing path
+        // Build a legend for the corpora
         this.buildLegend(svg, margin);
 
-        // Build a slider that highlight the current values
-        const focus = svg.append("g").style("display", "none");
-        const yLine = this.getYLine(focus, margin);
-        const circle1 = this.getHighlightCircle(focus, "darkorange", margin);
-        const circle2 = this.getHighlightCircle(focus, "steelblue", margin);
-        const rect = this.getMouseMoveRect(svg, margin, focus);
-        const move = function mousemove() {
-            var x0 = x.invert(d3.mouse(this)[0]),
-                i = Math.floor(x0),
-                d1 = trainingData[i] ? trainingData[i] : trainingData[0],
-                d2 = validationData[i] ? validationData[i] : validationData[0];
+        this.buildBar(margin, svg, corpus1, x, y, "#7F9DEE");
 
-            if (i < (trainingData.length) && i >= 0) {
-                circle1
-                    .attr("cx", x(i))
-                    .attr("cy", y(d1.y));
+        const h = this.state.height - margin.bottom - margin.top;
 
-                circle2
-                    .attr("cx", x(i))
-                    .attr("cy", y(d2.y));
-
-                yLine
-                    .attr("x1", x(i))
-                    .attr("x2", x(i));
-            }
-        };
-        rect.on("mousemove", move);
-
-        // Draw the lines
-        const transitionPath = this.defineTransitionPath();
-        this.buildPath(svg, margin, validationData, x, y, transitionPath, animate, "validation", "steelblue");
-        this.buildPath(svg, margin, trainingData, x, y, transitionPath, animate, "training", "darkorange");
+        svg.selectAll("rect")
+            .transition()
+            .duration(1000)
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            .attr("y", function(d) { return y(d.y); })
+            .attr("height", function(d) { return h - y(d.y); })
+            .delay(function(d,i){return(i*25)})
     }
 
 
@@ -159,9 +126,10 @@ class WordFrequencyGraph extends React.Component {
     }
 
     getX(data, margin) {
-        return d3.scaleLinear()
-            .domain([0, data.length - 1])
-            .range([0, this.state.width - margin.left - margin.right]);
+        return d3.scaleBand()
+            .domain(data.map(function(d) { return d.x; }))
+            .range([0, this.state.width - margin.left - margin.right])
+            .padding(0.5);
     }
 
     getY(data1, data2, margin) {
@@ -174,7 +142,7 @@ class WordFrequencyGraph extends React.Component {
         const max = Math.max(max1, max2);
 
         return d3.scaleLinear()
-            .domain([0, max * 1.05])
+            .domain([0, max])
             .range([this.state.height - margin.top - margin.bottom, 0]);
     }
 
@@ -207,27 +175,14 @@ class WordFrequencyGraph extends React.Component {
             .attr("transform", "translate(" + (margin.left) + ", " + 0 + ")")
             .attr("cx", 5).attr("cy", this.state.height - margin.bottom / 2 + 7)
             .attr("r", 6)
-            .style("fill", "darkorange");
-
-        svg.append("circle")
-            .attr("transform", "translate(" + (margin.left) + ", " + 0 + ")")
-            .attr("cx", 105).attr("cy", this.state.height - margin.bottom / 2 + 7)
-            .attr("r", 6)
-            .style("fill", "steelblue");
+            .style("fill", "#4972e5")
+            .style("fill-opacity", 0.8);
 
         svg.append("text")
             .attr("transform", "translate(" + (margin.left) + ", " + 0 + ")")
             .attr("x", 20)
             .attr("y", this.state.height - margin.bottom / 2 + 7)
-            .text("Training")
-            .style("font-size", "15px")
-            .attr("alignment-baseline", "middle");
-
-        svg.append("text")
-            .attr("transform", "translate(" + (margin.left) + ", " + 0 + ")")
-            .attr("x", 120)
-            .attr("y", this.state.height - margin.bottom / 2 + 7)
-            .text("Validation")
+            .text("Corpus 1")
             .style("font-size", "15px")
             .attr("alignment-baseline", "middle");
 
@@ -235,79 +190,24 @@ class WordFrequencyGraph extends React.Component {
             .attr("transform", "translate(" + (margin.left) + ", " + 0 + ")")
             .attr("x", (this.state.width - margin.right) - 100)
             .attr("y", this.state.height - margin.bottom / 2 + 7)
-            .text("Epochs")
+            .text("Words")
             .style("font-size", "15px")
             .attr("alignment-baseline", "middle");
     }
 
-    getYLine(focus, margin) {
-        return focus.append("line")
-            .attr("class", "x")
-            .style("stroke", "#d9d9dd")
-            .attr("y1", 0)
-            .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
-            .attr("y2", this.state.height - margin.top - margin.bottom);
-    }
+    buildBar(margin, svg, data, x, y, color) {
+        const h = this.state.height - margin.bottom - margin.top;
 
-    getHighlightCircle(focus, color, margin) {
-        return focus.append("circle")
-            .attr("class", "y")
-            .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+        svg.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.x); })
+            .attr("width", x.bandwidth())
             .style("fill", color)
-            .attr("r", 4.5);
-    }
-
-    getMouseMoveRect(svg, margin, focus) {
-        return svg.append("rect")
-            .attr("width", this.state.width - margin.left - margin.right + 10)
-            .attr("height", this.state.height - margin.top - margin.bottom)
-            .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
-            .style("fill", "none")
-            .style("pointer-events", "all")
-            .on("mouseover", function () {
-                focus.style("display", null);
-            })
-            .on("mouseout", function () {
-                focus.style("display", "none");
-            });
-    }
-
-    buildPath(svg, margin, data, x, y, transitionPath, animate, legend, color) {
-        const buildPath = svg.append("path")
-            .datum(data)
-            .attr("fill", "none")
-            .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
-            .attr("stroke", color)
-            .attr("stroke-opacity", 0.7)
-            .attr("data-legend", legend)
-            .attr("stroke-width", 2.0)
-            .attr("d", d3.line()
-                .x(function (d, i) {
-                    return x(i)
-                })
-                .y(function (d) {
-                    return y(d.y)
-                })
-            );
-
-        buildPath.on("mouseover", function (d) {
-            buildPath
-                .style("stroke-width", 3.0)
-                .style("stroke-opacity", 1.0);
-        }).on("mouseout", function (d) {
-            buildPath
-                .style("stroke-opacity", 0.7)
-                .style("stroke-width", 2.0);
-        });
-
-        if (animate) {
-            const pathLength = buildPath.node().getTotalLength();
-
-            buildPath.attr("stroke-dashoffset", pathLength)
-                .attr("stroke-dasharray", pathLength)
-                .transition(transitionPath)
-                .attr("stroke-dashoffset", 0);
-        }
+            .attr("y", function(d) { return y(0); })
+            .attr("height", function(d) {return h - y(0) });
     }
 
 
